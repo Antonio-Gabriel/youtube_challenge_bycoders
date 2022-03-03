8d596f1762cbe88b7ccf75546180cb9313236dc0
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-03-03 17:10:07 +0100

    validate private routes

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,20 +1,37 @@
-import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { signIn } from "../../redux/actions/authenticationAction";
 
 export function Login() {
   const dispatch = useDispatch();
+  const navigateTo = useNavigate();
+  const { accessToken, loading } = useSelector<any>(
+    (state) => state.auth
+  ) as any;
 
   function handleSignInWithGoogle() {
     dispatch(signIn());
   }
+
+  useEffect(() => {
+    if (accessToken) {
+      navigateTo("/");
+    }
+  }, [accessToken]);
 
   return (
     <section>
       <h2>Login Page</h2>
 
       <p>Sign in with google</p>
+      <span></span>
 
-      <button onClick={handleSignInWithGoogle}>Sign In with google</button>
+      {loading ? (
+        <span>loading...</span>
+      ) : (
+        <button onClick={handleSignInWithGoogle}>Sign In with google</button>
+      )}
     </section>
   );
 }
