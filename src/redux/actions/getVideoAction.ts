2df5cc45dcbb6b074d7aabccaca6f38b7c38be2b
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-03-04 09:28:22 +0100

    create action for get videos

---
+++ src/redux/actions/getVideoAction.ts
@@ -0,0 +1,35 @@
+import { getVideoServices } from "../../services/getVideosService";
+import { ENUMS_VIDEOS } from "./types/types";
+
+export const getVideoActions = () => async (dispatch: any) => {
+  try {
+    dispatch({
+      type: ENUMS_VIDEOS.REQUEST,
+    });
+
+    const response = await getVideoServices();
+
+    if (response) {
+      dispatch({
+        type: ENUMS_VIDEOS.SUCESS,
+        payload: {
+          videos: response.items,
+          nextPageToken: response.nextPageToken,
+          totalResult: response.pageInfo.totalResults,
+        },
+      });
+    } else {
+      dispatch({
+        type: ENUMS_VIDEOS.FAIL,
+        error: "Error for load the videos",
+      });
+    }
+  } catch (error: any) {
+    if (error) {
+      dispatch({
+        type: ENUMS_VIDEOS.FAIL,
+        error: error.message,
+      });
+    }
+  }
+};
