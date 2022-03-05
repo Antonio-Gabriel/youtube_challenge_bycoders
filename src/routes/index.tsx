4eb2262bca25817d1b3bb431bd46990a38c2654b
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-03-05 22:34:18 +0100

    add new route and enable query string support

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,7 @@ import { useRoutes, BrowserRouter as Router } from "react-router-dom";
 import { Home } from "../pages/Home";
 import { Login } from "../pages/Login";
 import { Watch } from "../pages/Watch";
+import { Search } from "../pages/Search";
 
 export function Routes() {
   const RoutesSwitch = () =>
@@ -18,7 +19,14 @@ export function Routes() {
         path: "/watch/:id",
         element: <Watch />,
       },
+      {
+        path: "/results",
+        element: <Search />,
+      },
     ]);
+
+  const queryString = "?search_query=:search";
+  new URLSearchParams(queryString);
 
   return (
     <Router>
