e369909cd5450f37bd60ac2feeeb0a68dcc61f03
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-03-05 13:44:47 +0100

    carries related videos in watch page

--- src/pages/Watch/index.tsx
+++ src/pages/Watch/index.tsx
@@ -1,11 +1,26 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import { getRelatedVideosActions } from "../../redux/actions/getRelatedVideosAction";
+import { IVideoPropsState } from "../../types/IVideoPropsState";
 
 export function Watch() {
   const { id } = useParams();
   const watchVideoUrl = `https://www.youtube.com/embed/${id}`;
 
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getRelatedVideosActions(id));
+  }, [dispatch]);
+
+  const { videos, loading, totalResult, nextPageToken } =
+    useSelector<IVideoPropsState>((state) => state.videos) as any;
+
   return (
     <section className="container">
+      {console.log(videos)}
+
       <div className="row">
         <div className="col-lg-8">
           <iframe
