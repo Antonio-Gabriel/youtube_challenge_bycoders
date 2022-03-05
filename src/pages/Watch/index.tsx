7ccd6e03c4f3be819e87ca5579444eb4b48bd6dc
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-03-05 12:58:51 +0100

    watch video page

--- src/pages/Watch/index.tsx
+++ src/pages/Watch/index.tsx
@@ -1,3 +1,27 @@
+import { useParams } from "react-router-dom";
+
 export function Watch() {
-  return <h1>Video Details</h1>;
+  const { id } = useParams();
+  const watchVideoUrl = `https://www.youtube.com/embed/${id}`;
+
+  return (
+    <section className="container">
+      <div className="row">
+        <div className="col-lg-8">
+          <iframe
+            style={{
+              width: "100%",
+              height: "400px !important",
+            }}
+            src={watchVideoUrl}
+            frameBorder={0}
+            title="video title"
+            allowFullScreen
+            width="1"
+          ></iframe>
+        </div>
+        <div className="col-lg-4"></div>
+      </div>
+    </section>
+  );
 }
