b5ab95d23e87bcec7fd8b952b0da9e3a3db7e5b3
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-03-05 08:57:40 +0100

    axios create instance

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,1 +1,10 @@
-export function api() {}
+import axios from "axios";
+
+const API_KEY = "AIzaSyBj2_2ygB8w_DwQR-PDfDPCkuUSDLqleQg";
+
+export const api = axios.create({
+  baseURL: "https://youtube.googleapis.com/youtube/v3/",
+  params: {
+    key: API_KEY,
+  },
+});
