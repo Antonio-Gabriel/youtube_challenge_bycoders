8db0f2df78209f3e11f22fcaaa2c0a53eae41725
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-03-05 22:32:42 +0100

    get all caches

---
+++ src/services/caching/getAllCachesInMemory.ts
@@ -0,0 +1,22 @@
+import { db } from "./database";
+
+type InMemoryCacheProps = {
+  id: string;
+  filterName: string;
+  userId: string;
+  createdAt: string;
+};
+
+function getCurrentUser() {
+  return JSON.parse(localStorage.getItem("bycoders-user") ?? "");
+}
+
+export async function getAllCachesInMemory() {
+  const histories = (await db.histories
+    .where({
+      userId: getCurrentUser().id,
+    })
+    .toArray()) as InMemoryCacheProps[];
+
+  return histories;
+}
