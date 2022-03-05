03cfe038e932657765c7e971dad70cb9e263a8f2
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-03-05 08:58:43 +0100

    add new feactures

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { signOut } from "../../redux/actions/authenticationAction";
@@ -7,19 +7,32 @@ import { IVideoPropsState } from "../../types/IVideoPropsState";
 
 import { formatDuration } from "../../utils/formatDuration";
 
+import numeral from "numeral";
+import InfiniteScroll from "react-infinite-scroll-component";
+
+import { getVideoServices } from "../../services/getVideosService";
+import { getUniqueElementsFromList } from "../../utils/getUniqueIdFromList";
+
 import "./styles.css";
 
 export function Home() {
   const dispatch = useDispatch();
   const navigateTo = useNavigate();
 
-  const { videos, loading, totalResult } = useSelector<IVideoPropsState>(
-    (state) => state.videos
-  ) as any;
+  const [valueList, setValueList] = useState<any>([]);
+  const [hasMore, setHasMore] = useState<boolean>(true);
 
   useEffect(() => {
     dispatch(getVideoActions());
   }, [dispatch]);
+
+  const { loading, totalResult, nextPageToken } = useSelector<IVideoPropsState>(
+    (state) => state.videos
+  ) as any;
+
+  useEffect(() => {
+    getVideoServices(nextPageToken).then((res) => setValueList(res.items));
+  }, []);
 
   function handleLogout() {
     dispatch(signOut());
@@ -28,11 +41,23 @@ export function Home() {
   }
 
   function handlePreviewVideoDetails(id: string) {
-    navigateTo(`/video-details/${id}`);
+    navigateTo(`/watch/${id}`);
   }
 
-  if (loading) {
-    return <h2>Loading...</h2>;
+  async function handleCarriesNextVideos() {
+    dispatch(getVideoActions());
+
+    const response = await getVideoServices(nextPageToken);
+
+    const margeVideoList = [...valueList, ...response.items];
+
+    setValueList([...getUniqueElementsFromList(margeVideoList, "id")]);
+
+    if (valueList.length === totalResult) {
+      setHasMore(false);
+    }
+
+    console.table(valueList);
   }
 
   return (
@@ -41,18 +66,33 @@ export function Home() {
       <span>
         <strong>Total Results: </strong> {totalResult}
       </span>
-
       <ul className="videos">
-        {videos.map((video: any) => (
-          <li
-            key={video.id}
-            onClick={() => handlePreviewVideoDetails(video.id)}
-          >
-            <img src={video.snippet.thumbnails.medium.url} />
-            <span>{video.snippet.title}</span>
-            <strong>{formatDuration(video.contentDetails.duration)}</strong>
-          </li>
-        ))}
+        <InfiniteScroll
+          dataLength={valueList.length}
+          next={handleCarriesNextVideos}
+          hasMore={hasMore}
+          loader={
+            <div className="spinner-border text-danger d-block mx-auto"></div>
+          }
+          endMessage={<h1>Final list</h1>}
+          className="videos"
+        >
+          {valueList.map((video: any) => (
+            <li
+              key={video.id}
+              onClick={() => handlePreviewVideoDetails(video.id)}
+            >
+              <img src={video.snippet.thumbnails.medium.url} />
+              <span>{video.snippet.title}</span>
+              <div className="video-footer">
+                <strong>{formatDuration(video.contentDetails.duration)}</strong>
+                <span>
+                  {numeral(video.statistics.viewCount).format("0.a")} views
+                </span>
+              </div>
+            </li>
+          ))}
+        </InfiniteScroll>
       </ul>
 
       <button onClick={handleLogout}>logout</button>
