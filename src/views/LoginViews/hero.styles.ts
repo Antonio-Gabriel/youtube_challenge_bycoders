0f9676d80174cac5f5fe6f4f61ccfa479ae74a06
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-03-06 06:24:05 +0100

    hero styles

---
+++ src/views/LoginViews/hero.styles.ts
@@ -0,0 +1,56 @@
+import styled from "styled-components";
+
+export const Content = styled.section`
+  .text-content {
+    display: flex;
+    flex-direction: column;
+
+    margin-top: 5rem;
+
+    img {
+      width: 120px;
+      height: 120px;
+    }
+
+    span {
+      font-weight: 900;
+      color: var(--red);
+
+      margin-bottom: 0.6rem;
+    }
+
+    h1 {
+      margin: 0;
+      padding: 0;
+      font-size: 2.6rem;
+      font-weight: 900;
+      max-width: 35rem;
+
+      margin-bottom: 1.2rem;
+    }
+
+    button {
+      font-family: "Roboto", sans-serif !important;
+      font-weight: 500;
+      padding: 14px 40px;
+      color: var(--white);
+
+      border: none;
+      background-color: #00bd4e;
+      border-radius: 8px;
+
+      margin-bottom: 6rem;
+      transition: filter 0.4s;
+
+      :hover {
+        filter: brightness(0.8);
+      }
+    }
+
+    p {
+      max-width: 32rem;
+      
+      color: var(--dark-600);
+    }
+  }
+`;
