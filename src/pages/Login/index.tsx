2ce3cda80baf5cacf8b7e82ae45caa770c2f2c5f
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-03-06 06:25:00 +0100

    update login page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,18 +1,13 @@
 import { useEffect } from "react";
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { signIn } from "../../redux/actions/authenticationAction";
+
+import { HeroView } from "../../views/LoginViews/Hero";
+import { LoginView } from "../../views/LoginViews/Login";
 
 export function Login() {
-  const dispatch = useDispatch();
   const navigateTo = useNavigate();
-  const { accessToken, loading } = useSelector<any>(
-    (state) => state.auth
-  ) as any;
-
-  function handleSignInWithGoogle() {
-    dispatch(signIn());
-  }
+  const { accessToken } = useSelector<any>((state) => state.auth) as any;
 
   useEffect(() => {
     if (accessToken) {
@@ -21,17 +16,9 @@ export function Login() {
   }, [accessToken]);
 
   return (
-    <section>
-      <h2>Login Page</h2>
-
-      <p>Sign in with google</p>
-      <span></span>
-
-      {loading ? (
-        <span>loading...</span>
-      ) : (
-        <button onClick={handleSignInWithGoogle}>Sign In with google</button>
-      )}
-    </section>
+    <>
+      <LoginView />
+      <HeroView />
+    </>
   );
 }
