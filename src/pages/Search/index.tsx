a40b6ca03486df5e7dc18992c61aae24052e0b12
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-03-06 14:32:11 +0100

    search page

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,7 +1,14 @@
+import { Results } from "../../views/Results";
+import { Header } from "../../components/Header";
 import { useSearchParams } from "react-router-dom";
 
 export function Search() {
   const [searchParams] = useSearchParams();
 
-  return <h2>{searchParams.get("search_query")}</h2>;
+  return (
+    <>
+      <Header filter={searchParams?.get("search_query") ?? ""} />
+      <Results />
+    </>
+  );
 }
