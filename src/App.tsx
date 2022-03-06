909546d2c186c619f7c95729dff4d6457d16616e
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-03-06 06:26:17 +0100

    add aos animations

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,13 @@ import store from "./redux/store";
 import { Routes } from "./routes";
 
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/global.scss";
+import "./styles/_normalize.scss";
+
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+AOS.init();
 
 function App() {
   return (
