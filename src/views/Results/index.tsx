8f1f3d79a909e4c7ab2d6b48d827cae6fb57b01b
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-03-07 18:44:34 +0100

    create video component

--- src/views/Results/index.tsx
+++ src/views/Results/index.tsx
@@ -1,5 +1,5 @@
-import ShowMoreText from "react-show-more-text";
-import { Container, Video, VideoContent } from "./styles";
+import { Container } from "./styles";
+import { Video } from "../../components/Video";
 
 export function Results() {
   return (
@@ -14,86 +14,19 @@ export function Results() {
             </span>
           </div>
           <div className="col-lg-12 results">
-            <Video>
-              <div className="image">
-                <img src="/src/assets/video.webp" alt="image name" />
-                <div className="duraction">
-                  <span>13:21</span>
-                </div>
-              </div>
-              <VideoContent>
-                <div className="title">
-                  <h5>
-                    Amapiano Quarantine Mix 2020 ( DJ Maphorisa | Kabza De Small
-                    | JazziDisciples | MFR Souls)
-                  </h5>
-
-                  <article>
-                    215 mil visualizações <strong></strong> há 1 ano
-                  </article>
-                </div>
-
-                <div className="channel">
-                  <img
-                    src="/src/assets/channels4_profile.jpg"
-                    alt="channel image"
-                  />
-                  <a href={`/results?search_query=`}>Freshly Baked</a>
-                </div>
-
-                <ShowMoreText
-                  className="description"
-                  lines={1}
-                  more=""
-                  less=""
-                  width={600}
-                >
-                  ut labore et dolore magna amet, consectetur adipiscing elit,
-                  sed do eiusmod tempor incididunt ut labore et dolore magna
-                  aliqua. Ut enim ad minim veniam,
-                </ShowMoreText>
-              </VideoContent>
-            </Video>
-            <Video>
-              <div className="image">
-                <img src="/src/assets/video.webp" alt="image name" />
-                <div className="duraction">
-                  <span>13:21</span>
-                </div>
-              </div>
-              <VideoContent>
-                <div className="title">
-                  <h5>
-                    Amapiano Quarantine Mix 2020 ( DJ Maphorisa | Kabza De Small
-                    | JazziDisciples | MFR Souls)
-                  </h5>
-
-                  <article>
-                    215 mil visualizações <strong></strong> há 1 ano
-                  </article>
-                </div>
-
-                <div className="channel">
-                  <img
-                    src="/src/assets/channels4_profile.jpg"
-                    alt="channel image"
-                  />
-                  <a href={`/results?search_query=`}>Freshly Baked</a>
-                </div>
-
-                <ShowMoreText
-                  className="description"
-                  lines={1}
-                  more=""
-                  less=""
-                  width={600}
-                >
-                  ut labore et dolore magna amet, consectetur adipiscing elit,
-                  sed do eiusmod tempor incididunt ut labore et dolore magna
-                  aliqua. Ut enim ad minim veniam,
-                </ShowMoreText>
-              </VideoContent>
-            </Video>
+            <Video
+              title="Amapiano Quarantine Mix 2020 ( DJ Maphorisa | Kabza De Small
+                | JazziDisciples | MFR Souls)"
+              duration="13:21"
+              thumbnail="/src/assets/video.webp"
+              views="215 mil"
+              publishedAt="há um ano"
+              channelName="Freshly Baked"
+              channelThumbnail="/src/assets/channels4_profile.jpg"
+              description="ut labore et dolore magna amet, consectetur adipiscing elit,
+              sed do eiusmod tempor incididunt ut labore et dolore magna
+              aliqua. Ut enim ad minim veniam,"
+            />
           </div>
         </div>
       </div>
