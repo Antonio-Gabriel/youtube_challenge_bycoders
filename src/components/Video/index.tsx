ffca517e8c20c4389db2cbc8da9a55e96e21bced
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-03-07 18:48:09 +0100

    video component

--- src/components/Video/index.tsx
+++ src/components/Video/index.tsx
@@ -1,8 +1,61 @@
+import ShowMoreText from "react-show-more-text";
+import { VideoContainer, VideoContent } from "./styles";
+
 type IVideoProps = {
+  href?: string;
   thumbnail: string;
   duration: string;
+  title: string;
+  views: string;
+  publishedAt: string;
+  channelThumbnail?: string;
+  channelName?: string;
+  description?: string;
 };
 
-export function Video() {
-  return <h1>video</h1>;
+export function Video({
+  href,
+  thumbnail,
+  title,
+  duration,
+  views,
+  publishedAt,
+  channelThumbnail,
+  channelName,
+  description,
+}: IVideoProps) {
+  return (
+    <VideoContainer href={href}>
+      <div className="image">
+        <img src={thumbnail} alt={title} />
+        <div className="duraction">
+          <span>{duration}</span>
+        </div>
+      </div>
+      <VideoContent>
+        <div className="title">
+          <h5>{title}</h5>
+
+          <article>
+            {views} views <strong></strong> {publishedAt}
+          </article>
+        </div>
+
+        <div className="channel">
+          <img src={channelThumbnail} alt={channelName} />
+          <a>{channelName}</a>
+        </div>
+
+        <ShowMoreText
+          className="description"
+          lines={1}
+          more=""
+          less=""
+          width={600}
+        >
+          {description}
+        </ShowMoreText>
+      </VideoContent>
+    </VideoContainer>
+  );
 }
