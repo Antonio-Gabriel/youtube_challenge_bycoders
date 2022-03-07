d0abd0309d2815c654c62775e5fd22f088c541f9
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-03-07 18:44:21 +0100

    result styles

--- src/views/Results/styles.ts
+++ src/views/Results/styles.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const Container = styled.section`
-  margin-top: 1rem;
+  margin-top: 6.6rem;
 
   .container {
     max-width: 1000px;
@@ -32,92 +32,5 @@ export const Container = styled.section`
     padding: 2px 0;
 
     margin-top: 0.6rem;
-  }
-`;
-
-export const Video = styled.a`
-  display: flex;
-  gap: 16px;
-  cursor: pointer;
-  margin-bottom: 10px;
-
-  color: var(--white) !important;
-
-  .image {
-    position: relative;
-    img {
-      max-width: 360px;
-      min-width: 240px;
-    }
-
-    .duraction {
-      position: absolute;
-      bottom: 10px;
-      right: 0;
-      margin: 4px;
-      height: 12px;
-
-      span {
-        font-size: 12px;
-        padding: 3px 4px;
-        background: rgb(4, 4, 4, 0.8);
-        border-radius: 2px;
-      }
-    }
-  }
-
-  :hover {
-    filter: brightness(0.8);
-  }
-`;
-
-export const VideoContent = styled.div`
-  display: flex;
-  flex-direction: column;
-
-  .title {
-    margin-bottom: 1rem;
-
-    h5 {
-      font-size: 1.2rem;
-      line-height: 1.6rem;
-      margin-bottom: 2px !important;
-    }
-    article {
-      display: flex;
-      align-items: center;
-      font-size: 0.8rem;
-      font-weight: 500;
-      color: var(--dark-400);
-
-      strong {
-        margin-right: 4px;
-      }
-    }
-  }
-
-  .channel {
-    display: flex;
-    align-items: center;
-    margin-bottom: 0.8rem;
-
-    img {
-      width: 24px;
-      height: 24px;
-      object-fit: cover;
-      border-radius: 50%;
-      margin-right: 6px;
-    }
-
-    a {
-      font-size: 12px;
-      color: var(--dark-400);
-    }
-  }
-
-  .description {
-    font-size: 12px;
-    color: var(--dark-400);
-    margin-bottom: 0.5rem;
   }
 `;
