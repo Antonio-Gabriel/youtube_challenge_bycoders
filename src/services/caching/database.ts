6e815a5fa79161d0d18dc5f98a44df2c12cb5f5f
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-03-08 22:21:29 +0100

    indexed filtername and user

--- src/services/caching/database.ts
+++ src/services/caching/database.ts
@@ -3,5 +3,5 @@ import Dexie from "dexie";
 export const db: Dexie | any = new Dexie("caching");
 
 db.version(1).stores({
-  histories: "++id, filterName, userId, createdAt",
+  histories: "++id, [filterName+userId], createdAt",
 });
