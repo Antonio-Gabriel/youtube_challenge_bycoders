4f506db4349605d11a61c53e7b2810472fb98878
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-03-09 15:04:23 +0100

    add results reducer in store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -3,12 +3,14 @@ import { composeWithDevTools } from "redux-devtools-extension";
 import { createStore, applyMiddleware, combineReducers } from "redux";
 import { authenticateReducer } from "./reducers/authenticationReducer";
 import { carriesVideosReducer } from "./reducers/CarriesVideosReducer";
+import { carriesResultsReducer } from "./reducers/CarriesResultsReducer";
 import { carriesRelatedVideosReducer } from "./reducers/CarriesRelatedVidoesReducer";
 
 const rootReducer = combineReducers({
   auth: authenticateReducer,
   videos: carriesVideosReducer,
   relatedVideos: carriesRelatedVideosReducer,
+  results: carriesResultsReducer
 });
 
 const store = createStore(
