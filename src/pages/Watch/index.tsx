a76ea0bdde6681daebcdae88e1c5d4a28aa03f76
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-03-09 15:04:44 +0100

    watching video

--- src/pages/Watch/index.tsx
+++ src/pages/Watch/index.tsx
@@ -1,10 +1,10 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import ShowMoreText from "react-show-more-text";
 import { Header } from "../../components/Header";
 import { useDispatch, useSelector } from "react-redux";
-import { AiFillLike, AiFillDislike } from "react-icons/ai";
 import { IVideoPropsState } from "../../types/IVideoPropsState";
+
 import {
   Channel,
   CurrentVideo,
@@ -12,14 +12,22 @@ import {
   Title,
   WatchContent,
 } from "./styles";
-import { getRelatedVideosActions } from "../../redux/actions/getRelatedVideosAction";
+
+import numeral from "numeral";
+
+import { ENUMS } from "../../redux/actions/types/types";
 import { SmallVideo } from "../../components/SmallVideo";
+import { getVideoById } from "../../services/getVideoById";
+import { getChannelByIdService } from "../../services/getChannelById";
+import { getRelatedVideosActions } from "../../redux/actions/getRelatedVideosAction";
 
 export function Watch() {
   const { id } = useParams();
-  const watchVideoUrl = `https://www.youtube.com/embed/${id}`;
 
   const dispatch = useDispatch();
+  const [videoToWatch, setVideoToWatch] = useState([]) as any;
+
+  const accessToken = localStorage.getItem("bycoders-accessToken") ?? "";
 
   useEffect(() => {
     dispatch(getRelatedVideosActions(id));
@@ -29,6 +37,40 @@ export function Watch() {
     (state) => state.videos
   ) as any;
 
+  useEffect(() => {
+    if (!accessToken) {
+      dispatch({
+        type: ENUMS.FAIL,
+        error: "Error",
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    getVideoById(id ?? "").then((res) => {
+      const joinVideoByChannelResponse = res.items.map(async (video: any) => {
+        const channelData = await getChannelByIdService(
+          video.snippet.channelId
+        );
+
+        return {
+          id: video.id,
+          duraction: video.contentDetails.duration,
+          title: video.snippet.title,
+          views: video.statistics.viewCount,
+          publishedAt: video.snippet.publishedAt,
+          description: video.snippet.description,
+          channelName: channelData.items[0].snippet.title,
+          channelThumbnail: channelData.items[0].snippet.thumbnails.default,
+        };
+      });
+
+      Promise.all(joinVideoByChannelResponse).then((res) =>
+        setVideoToWatch(res)
+      );
+    });
+  }, []);
+
   return (
     <>
       <Header />
@@ -37,67 +79,50 @@ export function Watch() {
         <section className="container">
           <div className="row">
             <div className="col-lg-8">
-              <CurrentVideo>
-                <iframe
-                  src={watchVideoUrl}
-                  frameBorder={0}
-                  title="video title"
-                  allowFullScreen
-                ></iframe>
+              {videoToWatch.map((video: any) => (
+                <CurrentVideo key={video.id}>
+                  <iframe
+                    src={`https://www.youtube.com/embed/${video.id}`}
+                    frameBorder={0}
+                    title={video.title}
+                    allowFullScreen
+                  ></iframe>
 
-                <Title>
-                  <article>
-                    <h5>
-                      Amapiano Quarantine Mix 2020 ( DJ Maphorisa | Kabza De
-                      Small | JazziDisciples | MFR Souls)
-                    </h5>
-                    <span>215 mil visualizações</span>
-                  </article>
-
-                  <ul>
-                    <li>
-                      <AiFillLike />
-                      200
-                    </li>
-
-                    <li>
-                      <AiFillDislike />2
-                    </li>
-                  </ul>
-                </Title>
-
-                <Channel>
-                  <div className="subscription">
+                  <Title>
                     <article>
-                      <img
-                        src="/src/assets/channels4_profile.jpg"
-                        alt="channel logo"
-                      />
-                      <div className="name">
-                        <strong>Freshly Baked</strong>
-                        <span>0.9980k Subscribers</span>
-                      </div>
+                      <h5>{video.title}</h5>
+                      <span>{numeral(video.views).format("0.a")} viws</span>
                     </article>
+                  </Title>
 
-                    <button>Subscribe</button>
-                  </div>
+                  <Channel>
+                    <div className="subscription">
+                      <article>
+                        <img
+                          src={video.channelThumbnail.url}
+                          alt={video.channelName}
+                        />
+                        <div className="name">
+                          <strong>{video.channelName}</strong>
+                          <span>Subscribers</span>
+                        </div>
+                      </article>
 
-                  <ShowMoreText
-                    className="description"
-                    lines={3}
-                    more="Show more"
-                    less="Show less"
-                    width={700}
-                  >
-                    ut labore et dolore magna amet, consectetur adipiscing elit,
-                    sed do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam,ut labore et dolore magna
-                    amet, consectetur adipiscing elit, sed do eiusmod tempor
-                    incididunt ut labore et dolore magna aliqua. Ut enim ad
-                    minim veniam Ut enim ad minim veniam
-                  </ShowMoreText>
-                </Channel>
-              </CurrentVideo>
+                      <button>Subscribe</button>
+                    </div>
+
+                    <ShowMoreText
+                      className="description"
+                      lines={3}
+                      more="Show more"
+                      less="Show less"
+                      width={700}
+                    >
+                      {video.description}
+                    </ShowMoreText>
+                  </Channel>
+                </CurrentVideo>
+              ))}
             </div>
             <div className="col-lg-4">
               <RelatedCourses>
@@ -118,18 +143,6 @@ export function Watch() {
                       channelThumbnail="/src/assets/channels4_profile.jpg"
                     />
                   </div>
-
-                  {/* {videos.map((video: any) => (
-                    <div className="col-lg-12">
-                      <div className="video-card">
-                        <img
-                          src={video.snippet?.thumbnails.medium.url}
-                          alt={video.snippet?.title}
-                        />
-                        <p>{video.snippet?.title}</p>
-                      </div>
-                    </div>
-                  ))} */}
                 </div>
               </RelatedCourses>
             </div>
